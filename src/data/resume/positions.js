6fa2f8e03c3bbd821a2a35977c6db2a00197d0df
mikhil123 <mikhilwankhede19@gmail.com>
2022-12-07 22:53:49 +0530

    environment value fixed

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -1,9 +1,23 @@
 const positions = [
   {
+    company: 'FIS',
+    position: 'Software Engineer II',
+    link: '',
+    daterange: 'October 2021 - Present',
+    points: [
+      'Use Java/J2EE technologies to develop settlement system web applications.',
+      'Developed highly interactive and customized UIs using JavaScript, HTML, JSP, and CSS to improve functionality of web applications.',
+      'Play a pivotal role in designing and implementing the pricing engine and risk management in transactions.',
+      'Mentored, coached and trained the software trainees.',
+      'Collaborate with different teams developers to implement solutions,resolve problems and perform code reviews.',
+      'Followed the development best practices such as version control, unit testing, continuous integration, performance, security testing and approriate documentations.',
+    ],
+  },
+  {
     company: 'Mosambee',
     position: 'Senior Software Engineer',
     link: '',
-    daterange: 'September 2018 - Present',
+    daterange: 'September 2018 - October 2021',
     points: [
       'Use Java/J2EE technologies to develop settlement system web applications.',
       'Developed highly interactive and customized UIs using JavaScript, HTML, JSP, and CSS to improve functionality of web applications.',
