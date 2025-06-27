7fe64b798d47a5f5ceb4b9a9135e8aad3f756eff
mikhil123 <mikhilwankhede19@gmail.com>
2025-06-27 17:32:31 +0530

    space added

--- src/data/resume/skills.js
+++ src/data/resume/skills.js
@@ -110,7 +110,7 @@ const skills = [
   {
     title: 'Apache Spark',
     competency: 3,
-    category: ['Languages','DevOps Tools'],
+    category: ['Languages', 'DevOps Tools'],
   },
 ].map((skill) => ({ ...skill, category: skill.category.sort() }));
 
